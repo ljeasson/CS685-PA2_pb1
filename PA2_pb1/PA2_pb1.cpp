91ddd2d83e97f49b3e40165c7e7e75bb4490e498
Lee Easson <ljeasson@gmail.com>
2020-02-24 18:17:35 -0800

    Add ReadImage.cpp and WriteImage.cpp Add find_affine_parameters() and calculate_pixel_error()

--- PA2_pb1/PA2_pb1.cpp
+++ PA2_pb1/PA2_pb1.cpp
@@ -7,10 +7,35 @@
 
 #include <iostream>
 
+#include "ReadImage.cpp"
+#include "WriteImage.cpp"
+
 using namespace std;
+
+void find_affine_parameters(int P1, int P2, int P3, int P4, int P1_F, int P2_F, int P3_F, int P4_F, int A[2][2], int b[2][1]);
+int calculate_pixel_error(int** img1, int** img2);
+
+void find_affine_parameters(int P1, int P2, int P3, int P4, int P1_F, int P2_F, int P3_F, int P4_F, int A[2][2], int b[2][1])
+{
+
+}
+
+int calculate_pixel_error(int** img1, int** img2)
+{
+    int error = 0;
+    return error;
+}
 
 int main()
 {
+    // Read in image
+    int** input, ** output;
+    int x_size, y_size, Q;
+    char name[20] = "./S1/1.pgm";
+    char outfile[25] = "./S1/1_transform.pgm";
+
+    ReadImage(name, &input, x_size, y_size, Q);
+
     // Manually extract acutal coordinates of 4 facial features from each image
     int P1, P2, P3, P4;
 
@@ -20,14 +45,14 @@ int main()
     // Computer parameters of affine transformation (A and b)
     int A[2][2];
     int b[2][1];
+    //find_affine_parameters(P1, P2, P3, P4, P1_F, P2_F, P3_F, P4_F, A, b);
 
     // Once parameters for affine transformation are found, 
     // normalize each image using computer affine transformation
 
+    WriteImage(outfile, output, x_size, y_size, Q);
+
     // Verify how well the computer affine transformation aligns 
     // by calculating average error in pixels
-    // For image
-    
-    // For set
-    
+        
 }
